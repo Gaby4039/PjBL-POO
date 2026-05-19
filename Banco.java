85faa5b83965964ec21816dbc77bb7b2ce4e39a8
gaby <gabrielly4039@gmail.com>
2026-05-18 22:24:24 -0300

    Classe Jogador atributos

---
+++ Banco.java
@@ -0,0 +1,1 @@
+// Criar função para distribuir dinheiro entre os jogadores a cada rodada
---
+++ Jogador.java
@@ -0,0 +1,15 @@
+public class Jogador {
+    private String cor;
+    private String nome;
+    private boolean faculdade;
+    private boolean profissao; 
+    private int casas;
+    private double salario;
+    private double saldo;
+    private boolean casamento; // true or false
+    private boolean seguro; // true or false
+}
+
+public Jogador(boolean faculdade) {
+
+}
