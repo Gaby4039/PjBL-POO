a00254b0dfd98bce09e58de7f758ffc70eafd77b
gsitoni <giuliasitoni@gmail.com>
2026-05-22 22:27:40 -0300

    Mudança de classe carta para classe abstrata

--- Carta.java
+++ Carta.java
@@ -1,3 +1,14 @@
-public class Carta {
-    
+public abstract class Carta {
+
+    protected String descricao;
+    protected int valor;
+
+    public Carta(String descricao, int valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    // Método abstrato
+    public abstract void mostrarCarta();
+
 }
