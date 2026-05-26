2e8a261d3e8080f5a27560d6d4541c252f47d989
gaby <gabrielly4039@gmail.com>
2026-05-25 22:45:57 -0300

    Criação das classes mais específicas da classe Carta

--- Casa.java
+++ Casa.java
@@ -1,18 +1,143 @@
-abstract class Casa {
+public abstract class Casa {
+    private String cor;
+    private String instrucao;
+
+    public Casa(String cor, String instrucao) {
+        this.cor = cor;
+        this.instrucao = instrucao;
+    }
+    
+    public abstract void setInstrucao(String instrucao);
 }
 
-class CasaSalario {
+class CasaSalario extends Casa {
+    public CasaSalario(String cor, String instrucao) {
+        super(cor, instrucao);
+    }
+
+    @Override
+    public void setInstrucao(String instrucao) {
+        
+    } 
+}
+
+class CasaImposto extends Casa {
+    public CasaImposto(String cor, String instrucao) {
+        super(cor, instrucao);
+    }
+
+    @Override
+    public void setInstrucao(String instrucao) {
+
+    }
+}
+
+class CasaSorte extends Casa {
+    public CasaSorte(String cor, String instrucao) {
+        super(cor, instrucao);
+    }
+
+    @Override
+    public void setInstrucao(String instrucao) {
+
+    }
+}
+
+class CasaAzar extends Casa {
+    public CasaAzar(String cor, String instrucao) {
+        super(cor, instrucao);
+    }
+
+    @Override
+    public void setInstrucao(String instrucao) {
+
+    }
+}
+
+// Classes mais genéricas herdando da base
+
+// CasaFamilia
+// CasaEvento
+// CasaProfissao
+
+// Classes mais específicas 
+
+class CasaDivida {
 
 }
 
-class CasaImposto {
+class CasaProfissao {
 
 }
 
-class CasaPremio {
+class CasaFaculdade {
 
 }
 
-class CasaAzar {
+class CasaPromocao {
+
+}
+
+class casaDemissao {
+
+}
+
+class CasaAumento {
+
+}
+
+class CasaCasamento {
+
+}
+
+class CasaFilho {
+
+}
+
+class CasaGemeos {
+
+}
+
+class CasaDivorcio {
+
+}
+
+class CasaAniversario {
+
+}
+
+class CasaVoltar {
+
+}
+
+class CasaAvancar {
+
+}
+
+class CasaPerdeTurno {
+
+}
+
+class CasaJogueNovamente {
+    
+}
+
+class CasaCompraPropriedade {
+
+}
+
+class CasaVendePropriedade {
+
+}
+
+class CasaAposentadoria {
+
+}
+
+class CasaFerias {
+
+}
+
+class CasaHospital {
 
 }
--- Tabuleiro.java
+++ Tabuleiro.java
@@ -4,8 +4,12 @@ public class Tabuleiro {
     private ArrayList<Casa> casas;
     private int quantidadeCasas;
 
-    public Tabuleiro() {
+    public Tabuleiro(int quantidadeCasas) {
         ArrayList<Carta> cartas = new ArrayList<>(quantidadeCasas);
+    }
+
+    public ArrayList<Casa> getCasas() {
+        return casas;
     }
 
 }
